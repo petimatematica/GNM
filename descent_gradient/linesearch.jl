677f01bb5ee7712992ed5ed51b6c081ad5e521db
clarab2004 <clarabrito2004@gmail.com>
2023-12-01 11:14:00 -0300

    modificação

--- descent_gradient/linesearch.jl
+++ descent_gradient/linesearch.jl
@@ -25,3 +25,24 @@ function armijo(x,f,gfx,sigma,stpmin)
     end
 end
 
+#
+# Goldstein
+# 
+function goldstein(x,f,gfx,sigma,alpha,beta,stpmin)   
+    error = 0
+    stp = 1.0
+    fx = f(x)
+    while true
+        gtg = sigma * gfx' * gfx
+        p = x + stp * gfx 
+        fp = f(p) 
+        steptestone = fp - fx - alpha * sigma * gtg
+        steptestwo = fp -fx - beta * sigma * gtg
+        if steptestone < 0.0 < steptestwo
+            stp = stp / 2.0
+        else 
+            return (stp, p, error) 
+        end    
+
+    end
+end
