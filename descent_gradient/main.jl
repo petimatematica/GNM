b8759c0de4728d55474b1a7cc53bdcd82cf343b5
clarab2004 <clarabrito2004@gmail.com>
2023-12-13 11:57:50 -0300

    Update main.jl
    modificações

--- descent_gradient/main.jl
+++ descent_gradient/main.jl
@@ -18,10 +18,10 @@ f(x) = rosenbrock2(x)
 # Setting gradient of objective function
 g(x) = grad_rosenbrock2(x)
 
-x0 = [2.0;-2.0]
-epsilon = 1.e-8
+x0 = [-1.2;1.0]
+epsilon = 1.e-9
 maxiter = 1000
-(x,ngfx,iter,error) = descentgradient(x0,f,g,epsilon,maxiter)
+(x,ngfx,iter,error) = descentgradient(x0,f,g,epsilon,maxiter,goldstein)
 
 
 
